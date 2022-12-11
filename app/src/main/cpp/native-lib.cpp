81be28a644a790cc1cd9526a028589fbc12c9179
gandharva123 <120114664+gandharva123@users.noreply.github.com>
2022-12-11 15:28:27 +0530

    Rohan added

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ JNIEXPORT jstring JNICALL
 Java_rishz_arandroid_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Rohan";
     return env->NewStringUTF(hello.c_str());
 }
